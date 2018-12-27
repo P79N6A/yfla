b0200f669394579d0ce5d8235cd46fec0e04a37c
iweizhong <1994927wz>
2018-12-27 18:19:33 +0800

    commit

---
+++ src/test/java/weimall/weimallfrontweb/WeimallFrontWebApplicationTests.java
@@ -0,0 +1,17 @@
+package weimall.weimallfrontweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeimallFrontWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
